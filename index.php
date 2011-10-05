b67c0e401fbd3b965ceb57e6fe96c7e7b544b29f
cwatson <cwatson@wattzup.com>
2011-10-05 15:34:15 +0000

    No changes yet...

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/****
+* Penguin CMS written by Christopher Watson
+* Version 1.0 BETA
+* 09/02/2011
+*
+* License GNU (GENERAL PUBLIC LICENSE) http://www.gnu.org/copyleft/gpl.html
+****/
+
+// Load the script that makes everything happen
+require_once('./cms_header.php');
+
+?>
