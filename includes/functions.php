e5eb1943d4c3ac10e005a8ea9d6fe425d9089a37
cwatson@wattzup.com <cwatson@wattzup.com@0f4277d1-b77b-5db0-d473-a1fb06198552>
2011-10-05 15:38:14 +0000

    No changes yet...

---
+++ includes/functions.php
@@ -0,0 +1,47 @@
+<?php
+
+function getOption($optionName) {
+	$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DB);
+		if($result = $mysqli->query('SELECT option_value from options WHERE option_name = "'.$optionName.'"')) {
+			while ($row = $result->fetch_object()) {
+				return $row->option_value;
+			}
+		}
+		else {
+			echo $mysqli->error;
+		}
+	$mysqli->close;
+}
+function getPage($id,$type='content') {
+	$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DB);
+		if($result = $mysqli->query('SELECT ' . $type . ' from pages WHERE id = "'.$id.'"')) {
+			while ($row = $result->fetch_object()) {
+				return $row->$type;
+			}
+		}
+		else {
+			echo $mysqli->error;
+		}
+	$mysqli->close;
+}
+function curDir() {
+	$path = dirname($_SERVER['PHP_SELF']);
+	$position = strpos($path,'/') + 1;
+	return ( '../' .substr($path,$position) );
+}
+define (TEMPDIR, curDir() . getOption('template_dir'));
+
+function insertLogo($width="300",$height="150") {
+	$url = getOption('logo_url');
+	return print '<img src='.$url.' width="'.$width.'" height="'.$height.'" />';
+}
+
+function getSidebar() {
+	if (getOption('sidebar_active') == TRUE) {
+		include (TEMPDIR . '/sidebar.php');
+	}
+	else {
+		
+	}
+}
+?>
