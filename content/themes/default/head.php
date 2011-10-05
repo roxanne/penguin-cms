2e9cbcd3edf50b588137d96392584e141e5790f0
cwatson <cwatson@wattzup.com>
2011-10-05 15:40:50 +0000

    No changes yet...

---
+++ content/themes/default/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php print getOption('site_name') ?> | <?php print getOption('site_description') ?></title>
+<link href="<?php print TEMPDIR; ?>/style.css" rel="stylesheet" type="text/css" media="screen" />
+
+<?php if (getOption('google_analytics') != '') { print getOption('google_analytics'); } ?>
+</head>
+<body>
+<div id="fullWrap">
+
+<div id="head">
+<span class="logo">
+<?php insertLogo(); ?>
+</span>
+<div id="navWrap">
+
+</div>
+</div>
+
+<div id="mainContent">
